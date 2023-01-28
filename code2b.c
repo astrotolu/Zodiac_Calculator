89c972f0b60ba13ec0fed8e2c09457a5161619a9
Alade Toluwanimi <astrogirl788@gmail.com>
2023-01-28 23:13:28 +0100

    Debugged error concerning astro_sign

--- code2b.c
+++ code2b.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char zodiac_sign(int, char *); // check zodiac sign
-// void converttoLC(char *); //convert month to lower case
+char *zodiac_sign(int, char *); // check zodiac sign
 
 int main()
 {
@@ -13,24 +12,11 @@ int main()
 
     printf("\nEnter your birth day: ");
     scanf("%d", &day);
-    printf("\nEnter ypur birth month: ");
+    printf("\nEnter your birth month: ");
     scanf("%s", &month);
 
     printf("\nYour zodiac sign is  %s", zodiac_sign(day, month));
 }
-
-// void converttoLC(char *month)
-// {
-//     int x;
-
-//     for (x = 0; x < 15; x++)
-//     {
-
-//         month[x] = tolower(month[x]);
-//     }
-
-//     return month[x];
-// }
 
 #define DECEMBER "december"
 #define JANUARY "january"
@@ -45,178 +31,75 @@ int main()
 #define OCTOBER "october"
 #define NOVEMBER "november"
 
-char zodiac_sign(int day, char *month)
+char *zodiac_sign(int day, char *month)
 {
-    // int day = 0;
-    // char month[12] =
 
-    // converttoLC(month);
+    char *astro_sign = NULL;
 
-    char *astro_sign = {'\0'};
-
-    if (strcmp(month, DECEMBER) == 0)
+    if ((strcmp(month, NOVEMBER) == 0 && day >= 22) || (strcmp(month, DECEMBER) == 0 && day <= 21))
     {
 
-        if (day < 22)
-        {
-
-            astro_sign = "Sagittarius";
-        }
-        else
-        {
-            astro_sign = "Capricorn";
-        }
+        astro_sign = "Sagittarius";
     }
 
-    else if (strcmp(month, JANUARY) == 0)
+    else if ((strcmp(month, DECEMBER) == 0 && day >= 22) || (strcmp(month, JANUARY) == 0 && day <= 19))
     {
 
-        if (day < 20)
-        {
-
-            astro_sign = "Capricorn";
-        }
-        else
-        {
-            astro_sign = "Aquarius";
-        }
+        astro_sign = "Capricorn";
     }
-    else if (strcmp(month, FEBRUARY) == 0)
+    else if ((strcmp(month, JANUARY) == 0 && day >= 20) || (strcmp(month, FEBRUARY) == 0 && day <= 18))
     {
 
-        if (day < 19)
-        {
-
-            astro_sign = "Aquarius";
-        }
-        else
-        {
-
-            astro_sign = "pisces";
-        }
+        astro_sign = "Aquarius";
     }
-    else if (strcmp(month, MARCH) == 0)
+    else if ((strcmp(month, FEBRUARY) == 0 && day >= 19) || (strcmp(month, MARCH) == 0 && day <= 20))
     {
 
-        if (day < 21)
-        {
-
-            astro_sign = "pisces";
-        }
-        else
-        {
-            astro_sign = "aries";
-        }
+        astro_sign = "pisces";
     }
-    else if (strcmp(month, APRIL) == 0)
+    else if ((strcmp(month, MARCH) == 0 && day >= 21) || (strcmp(month, APRIL) == 0 && day <= 19))
     {
 
-        if (day < 20)
-        {
-
-            astro_sign = "aries";
-        }
-        else
-        {
-
-            astro_sign = "taurus";
-        }
+        astro_sign = "aries";
     }
-    else if (strcmp(month, MAY) == 0)
+    else if ((strcmp(month, APRIL) == 0 && day >= 20) || (strcmp(month, MAY) == 0 && day <= 20))
     {
 
-        if (day < 21)
-        {
-
-            astro_sign = "taurus";
-        }
-        else
-        {
-
-            astro_sign = "gemini";
-        }
+        astro_sign = "taurus";
     }
-    else if (strcmp(month, JUNE) == 0)
+    else if ((strcmp(month, MAY) == 0 && day >= 21) || (strcmp(month, JUNE) == 0 && day <= 21))
     {
-
-        if (day < 21)
-        {
-
-            astro_sign = "gemini";
-        }
-        else
-        {
-
-            astro_sign = "cancer";
-        }
+        astro_sign = "gemini";
     }
-    else if (strcmp(month, JULY) == 0)
+    else if ((strcmp(month, JUNE) == 0 && day >= 22) || (strcmp(month, JULY) == 0 && day <= 22))
     {
 
-        if (day < 23)
-        {
-
-            astro_sign = "cancer";
-        }
-        else
-        {
-            astro_sign = "leo";
-        }
+        astro_sign = "cancer";
     }
-    else if (strcmp(month, AUGUST) == 0)
+    else if ((strcmp(month, JULY) == 0 && day >= 23) || (strcmp(month, AUGUST) == 0 && day <= 22))
     {
 
-        if (day < 23)
-        {
-
-            astro_sign = "leo";
-        }
-        else
-        {
-            astro_sign = "virgo";
-        }
+        astro_sign = "leo";
     }
-    else if (strcmp(month, SEPTEMBER) == 0)
+    else if ((strcmp(month, AUGUST) == 0 && day >= 23) || (strcmp(month, SEPTEMBER) == 0 && day <= 22))
     {
 
-        if (day < 23)
-        {
-
-            astro_sign = "virgo";
-        }
-        else
-        {
-
-            astro_sign = "libra";
-        }
+        astro_sign = "virgo";
     }
-    else if (strcmp(month, OCTOBER) == 0)
+    else if ((strcmp(month, SEPTEMBER) == 0 && day >= 23) || (strcmp(month, OCTOBER) == 0 && day <= 23))
     {
 
-        if (day < 23)
-        {
-
-            astro_sign = "libra";
-        }
-        else
-        {
-
-            astro_sign = "scorpio";
-        }
+        astro_sign = "libra";
     }
-    else if (strcmp(month, NOVEMBER) == 0)
+    else if ((strcmp(month, OCTOBER) == 0 && day >= 24) || (strcmp(month, NOVEMBER) == 0 && day <= 21))
     {
 
-        if (day < 22)
-        {
+        astro_sign = "scorpius";
+    }
+    else
+    {
 
-            astro_sign = "scorpio";
-        }
-        else
-        {
-
-            astro_sign = "sagittarius";
-        }
+        return printf("\nUndefined\n");
     }
 
     return astro_sign;
